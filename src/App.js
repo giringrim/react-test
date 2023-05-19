62228c3622cfabe9ca4de6a83d6cda3fae95e7ff
기린그림 이보람 <lbr9932@giringrim.com>
2023-05-19 13:39:27 +0900

    퍼블수정20230519

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          퍼블수정2 Edit <code>src/App.js</code> and save to reload.
+          퍼블수정20230519 Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
