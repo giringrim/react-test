35f02ad223d7456348d12dd144f3e6b5e12f7324
JH <ruados12@gmail.com>
2023-05-25 17:12:49 +0900

    230525

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          퍼블수정20230523 테스트 입니다. Edit <code>src/App.js</code> and save to reload.
+          퍼블수정20230523 테스트 입니다. Edit <code>src/App.js</code> and save to reload. v.230525
         </p>
         <a
           className="App-link"
